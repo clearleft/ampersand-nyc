33b7c3c0e879670917bfeb9bcb3e8f83712d4a39
Richard Rutter <rich@clagAir-2.local>
2013-10-27 14:52:45 +0000

    updated handfj link

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,28 @@
 <!-- ###############  FOOTER ################### -->
-		
+
 		<!-- SPONSORS -->
 		<section class="sponsors">
 			<header class="section clearfix">
 				<h1>Our Generous Sponsors</h1>
-			</header>	
+			</header>
 				<div class="premiere">
 					<h2><span>Premiere Sponsors</span></h2>
 					<a href="http://fontdeck.com"><img src="images/sponsors-fontdeck.svg" alt="Fontdeck"></a>
 					<a href="http://typekit.com"><img src="images/sponsors-typekit.svg" alt="Typekit" style="position:relative; top:-7px"></a>
-				
+
 				</div>
 				<div class="executive">
 					<h2><span>Executive Sponsors</span></h2>
-					<a href="http://www.typography.com/index.php?affiliateID=312"><img src="images/sponsors-hoefler-frere-jones.svg" alt="Hoefler &amp; Frere-Jones" style="position:relative; top:-5px;"></a>
+					<a href="http://www.typography.com/cloud/welcome/?affiliateID=516"><img src="images/sponsors-hoefler-frere-jones.svg" alt="Hoefler &amp; Frere-Jones" style="position:relative; top:-5px;"></a>
 					<a href="http://gridsetapp.com/"><img src="images/sponsors-gridset.svg" alt="Gridset" style="position:relative; top:-30px"></a>
-					<a href="http://myfonts.com"><img src="images/sponsors-myfonts.svg" alt="MyFonts" style="position:relative; top:-1px"></a>	
-					<a href="http://webtype.com"><img src="images/sponsors-webtype.svg" alt="WebType" style="position:relative; top:-32px; height: 35px;"></a>	
-					<a href="http://www.ffmark.com/"><img src="images/sponsors-fontfont.svg" alt="FontFont FF Mark" style="position:relative; top:-20px; width:180px"></a>	
+					<a href="http://myfonts.com"><img src="images/sponsors-myfonts.svg" alt="MyFonts" style="position:relative; top:-1px"></a>
+					<a href="http://webtype.com"><img src="images/sponsors-webtype.svg" alt="WebType" style="position:relative; top:-32px; height: 35px;"></a>
+					<a href="http://www.ffmark.com/"><img src="images/sponsors-fontfont.svg" alt="FontFont FF Mark" style="position:relative; top:-20px; width:180px"></a>
 				</div>
 				<div class="media-partners">
-					<h2><span>Media Partners</span></h2>	
-					<a href="http://www.fivesimplesteps.com/"><img src="images/sponsors-five-simple-steps.svg" alt="Five Simple Steps" style="background:#fff"></a>	
-					<a href="http://tdc.org/"><img src="images/sponsors-TDC.png" alt="Type Directors Club" style="height: 85px;position:relative; top:3px"></a>				
+					<h2><span>Media Partners</span></h2>
+					<a href="http://www.fivesimplesteps.com/"><img src="images/sponsors-five-simple-steps.svg" alt="Five Simple Steps" style="background:#fff"></a>
+					<a href="http://tdc.org/"><img src="images/sponsors-TDC.png" alt="Type Directors Club" style="height: 85px;position:relative; top:3px"></a>
 				</div>
 				<div class="grass-roots">
 					<h2><span>Grass Roots Sponsors</span></h2>
@@ -32,9 +32,9 @@
 				</div>
 			</div>
 		</section>
-	
-		
-	
+
+
+
 		<!-- SOCIAL -->
 		<section class="section social">
 			<h3>Keep updated with the latest Ampersand activity</h3>
@@ -44,7 +44,7 @@
 				<li><a id="lanyrd" href="http://lanyrd.com/2013/ampersand-nyc/">Lanyrd </a></li>
 				<li><a id="ical" href="webcal://h2vx.com/ics/nyc.ampersandconf.com/schedule">+&nbsp;iCal </a></li>
 			</ul>
-		</section>   
+		</section>
 
 		<!-- FOOTER NAV -->
 		<div class="section" >
@@ -55,9 +55,9 @@
 					<li><a class="accessibility" href="/accessibility.php">Accessibility</a></li>
 				</ul>
 			</nav>
-		
+
 		</div>
-	
+
 		<!-- CLEARLEFT FOOTER -->
 		<footer >
 			<div class="section">
@@ -66,6 +66,6 @@
 				Email us on <a href="mailto:ampersand@clearleft.com">ampersand@clearleft.com</a>
 				</p>
 				<small>Ampersand &copy; 2013 Clearleft Ltd. All rights reserved. Set in <a href="http://fontdeck.com/typeface/karmina">Karmina</a> and <a href="http://fontdeck.com/typeface/karminasans">Karmina Sans</a> served by <a href="http://fontdeck.com/">Fontdeck.</a></small>
-			
+
 			</div>
 		</footer>
